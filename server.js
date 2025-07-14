9d1e3b31da8110aba0e084f619b66b98fce2a3c0
Gonzalo De Castro <gonzalodecastro1q@gmail.com>
2025-07-14 17:43:31 -0300

    Version added in console

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 3001;
+const version = require("./package.json").version;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} version ${version}`);
 });
